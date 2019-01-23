00b4d1431fd1421d2e8f0bbafbe8f42beb5095dd
l.marchand <l.marchand@net.estia.fr>
2019-01-23 15:00:12 +0100

    Réorganisation et Remplissage roleMBDS avec messages et users

--- grails-app/init/tp1_v2/BootStrap.groovy
+++ grails-app/init/tp1_v2/BootStrap.groovy
@@ -9,18 +9,17 @@ import fr.mbds.tp.UserRole
 class BootStrap {
 
     def init = { servletContext ->
+//        user et role ADMIN
         def userAdmin = new User(username:"admin",password:"secret",firstName:"admin",lastName:"admin",mail:"admin@mali.com").save(flush:true,failOnError:true)
         def roleAdmin = new Role(authority:"ROLE_ADMIN").save(flush:true,failOnError:true)
-        def rolePromo2019 = new Role(authority:"ROLE_PROMO2019").save(flush:true,failOnError:true)
-        new Role(authority:"ROLE_MBDS").save(flush:true,failOnError:true)
         UserRole.create(userAdmin,roleAdmin,true)
 
-
-        (1..50).each {
+//        role, user et message pour Promo2019
+        def rolePromo2019 = new Role(authority:"ROLE_PROMO2019").save(flush:true,failOnError:true)
+        (1..25).each {
             def userInstance = new User(username:"username-$it",password:"password-$it",firstName:"first-$it", lastName:"last-$it",mail:"mail-$it").save(flush:true,failOnError:true)
 
-
-            new Message(messageContent: "lala",author: userInstance).save(flush:true,failOnError:true)
+            new Message(messageContent: "coucou, je suis ${userInstance.username}",author: userInstance).save(flush:true,failOnError:true)
 
             UserRole.create(userInstance,rolePromo2019,true)
         }
@@ -33,6 +32,18 @@ class BootStrap {
                         UserMessage.create(userInstance,messageInstance,true)
                 }
         }
+
+//      role MBDS et message, pour les 5 premiers users
+        def roleMBDS = new Role(authority:"ROLE_MBDS").save(flush:true,failOnError:true)
+        def messageInstanceMBDS =  new Message(messageContent: "Bienvenue en MBDS",author: userAdmin).save(flush:true,failOnError:true)
+        User.list([max:5]).each {
+            User userInstance ->
+                UserRole.create(userInstance,roleMBDS,true)
+                UserMessage.create(userInstance,messageInstanceMBDS, true)
+        }
+
+
+
 
 //        def userInstance = new User(username:"username",password:"password",firstName:"first", lastName:"last",mail:"mail")
 //        def userInstance2 = new User(username:"username2",password:"password2",firstName:"first2", lastName:"last2",mail:"mail2")
